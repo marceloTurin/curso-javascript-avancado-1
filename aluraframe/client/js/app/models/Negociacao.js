317d59e5ea483da4dbddb9b35fedd5e3941df8b6
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-04-01 19:07:21 -0300

    Aula 2: A classe negociação

---
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,9 @@
+//Criação da Classe Negociacao
+class Negociacao {
+	//Criação do construtor onde colocamos os atributos da classe
+	constructor(){
+		this.data = new Date();
+		this.quantidade = 1;
+		this.valor = 0.0;
+	}
+}
