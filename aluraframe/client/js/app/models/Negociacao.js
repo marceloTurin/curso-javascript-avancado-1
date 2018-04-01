9337ac257b2265a6e9aad9252a9a0c1e22184714
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-04-01 19:54:02 -0300

    Aula 2: Encapsulamento

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,14 +1,31 @@
 //Criação da Classe Negociacao
+
+// atributos com _ não pode ser aessado fora da classe
 class Negociacao {
 	//Criação do construtor onde colocamos os atributos da classe
 	constructor(data,quantidade,valor){
-		this.data = data;
-		this.quantidade = quantidade;
-		this.valor = valor;
+		this._data = data;
+		this._quantidade = quantidade;
+		this._valor = valor;
 	}
 
 	//Metodo que calcula o volume
-	obtemVolume(){
-		return this.quantidade * this.valor;
+	getVolume(){
+		return this._quantidade * this._valor;
+	}
+
+	//Retorna a data
+	getData(){
+		return this._data;
+	}
+
+	//Retorna a quantidade
+	getQuantidade(){
+		return this._quantidade;
+	}
+
+	//Retorna o valor
+	getValor(){
+		return this._valor;
 	}
 }
