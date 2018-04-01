408eb5929f7f8f7edda24093d569206153e595de
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-04-01 19:21:12 -0300

    Aula 2: Construtor de classe e métodos

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,9 +1,14 @@
 //Criação da Classe Negociacao
 class Negociacao {
 	//Criação do construtor onde colocamos os atributos da classe
-	constructor(){
-		this.data = new Date();
-		this.quantidade = 1;
-		this.valor = 0.0;
+	constructor(data,quantidade,valor){
+		this.data = data;
+		this.quantidade = quantidade;
+		this.valor = valor;
+	}
+
+	//Metodo que calcula o volume
+	obtemVolume(){
+		return this.quantidade * this.valor;
 	}
 }
