4375b3604662c1ce5e893bb6368a7b22e2e62984
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-04-07 22:41:11 -0300

    Associoando métodos do controller ás ações do usuário

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,6 +2,16 @@ class NegociacaoController {
 
 	adiciona(event){
 		event.preventDefault();
-		alert("Chamei ação no controller");
+		
+		//Criação da variavel $ que liga o querySelector ao document
+		let $ = document.querySelector.bind(document);
+
+		let inputData = $("#data");
+		let inputQuantidade = $("#quantidade");
+		let inputValor = $("#valor");
+
+		console.log(inputData.value);
+		console.log(inputValor.value);
+		console.log(inputQuantidade.value);
 	}
 }
