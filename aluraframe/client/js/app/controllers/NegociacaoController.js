e8ab6e80bb9b5db70a6e3231fa8687ad16b38983
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-04-07 23:51:34 -0300

    Aula 3: Criando uma instância de Negociacao

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,15 +7,21 @@ class NegociacaoController {
 		let $ = document.querySelector.bind(document);
 
 		//Selecão dos input do formulário
-		this.inputData = $("#data");
-		this.inputQuantidade = $("#quantidade");
-		this.inputValor = $("#valor");
+		this._inputData = $("#data");
+		this._inputQuantidade = $("#quantidade");
+		this._inputValor = $("#valor");
 	}
 
 	adiciona(event){
 		event.preventDefault();
-		console.log(this.inputData.value);
-		console.log(this.inputValor.value);
-		console.log(this.inputQuantidade.value);
+		
+		console.log(typeof(this._inputData.value)); // Ver o tipo de valor da variavel
+		console.log(this._inputData.value);
+
+		let negociacao = new Negociacao{
+			this._inputData.value,
+			this._inputQuantidade.value,
+			this._inputData.value
+		}
 	}
 }
