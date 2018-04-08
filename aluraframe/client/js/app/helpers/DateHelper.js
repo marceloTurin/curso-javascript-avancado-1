9128eb849526bc65262a5564649d5be05dd015b3
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-04-08 19:18:25 -0300

    Aula 4: Template String

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -9,19 +9,24 @@ class DateHelper{
 	static dataParaTexto(data){
 
 		//Exibe a data no formato dia/mês/ano
-		return data.getDate() 
-			+ '/' + (data.getMonth() + 1)
-			+ '/' + data.getFullYear();
+		return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+
 	}
 
 	//Metodo estatico
 	static textoParaData(texto){
 
-		//Tranforma a String em um array dividindo os valores com o split quando o parametro for - 
-		//A função map percorre cada item do array permetindo você fazer algo com o item
-		//Com => criamos uma função Arrow Functions não precisando declarar a function 
+		
+		//Expressão regular para verificar se a data esta no formato correto
+		 if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
+		 	throw new Error('Deve estar no formato aaaa-mm-dd');
 
+
+		 //Tranforma a String em um array dividindo os valores com o split quando o parametro for - 
+		 //A função map percorre cada item do array permetindo você fazer algo com o item
+		 //Com => criamos uma função Arrow Functions não precisando declarar a function 
 		 return new Date (...texto.split('-').map((item,indice) => item - indice % 2));
+
 
 
 		 //O mesmo exemplo usando replace com expressão regular trocando o - pela virgula
