548fa96823886590fbc6beeec2f9d6547ad3d518
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-04-08 15:12:09 -0300

    Aula 3: Arrow Functions: deixando o código menos verboso

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -19,20 +19,27 @@ class NegociacaoController {
 		console.log(this._inputData.value);
 
 		//Tranforma a String em um array dividindo os valores com o split quando o parametro for - 
+		//A função map percorre cada item do array permetindo você fazer algo com o item
+		//Com => criamos uma função Arrow Functions não precisando declarar a function 
+
 		let data = new Date(...
 			this._inputData.value
-			.split('-')
-			.map(function(item,indice){ //A função map percorre cada item do array permetindo você fazer algo com o item
-				
-				return item - indice % 2;
-			})
+			.split('-')  
+			.map((item,indice) => item - indice % 2)
 		);
 
 		//O mesmo exemplo usando replace com expressão regular trocando o - pela virgula
 		//let data = new Date(this._inputData.value.replace(/-/g, ','));
 
-		console.log(data);
 
+		let negociacao = new Negociacao(
+			data,
+			this._inputQuantidade.value,
+			this._inputValor.value
+			);
+
+		console.log(negociacao);
+		
 
 	}
 }
