46baa66a76f5729955de939e089045578d0d60ee
Marcelo da Silva Sousa <marcelo.sousa1997@hotmail.com>
2018-04-08 00:07:39 -0300

    Aula 3: Criando um objeto Date

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -18,6 +18,16 @@ class NegociacaoController {
 		console.log(typeof(this._inputData.value)); // Ver o tipo de valor da variavel
 		console.log(this._inputData.value);
 
+		//Tranforma a String em um array dividindo os valores com o split quando o parametro for - 
+		let data = new Date(this._inputData.value.split('-'));
+
+		//O mesmo exemplo usando replace com expressão regular trocando o - pela virgula
+		let data = new Date(this._inputData.value.replace(/-/g, ','));
+
+		console.log(data);
+
+
+
 		let negociacao = new Negociacao{
 			this._inputData.value,
 			this._inputQuantidade.value,
